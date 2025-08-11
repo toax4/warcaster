a2175fb994f7416140263cfe45bf92cfe1cc7c3a
BOCKELANDT Adrien <adrien.bock83@gmail.com>
2025-08-11 16:16:29 +0200

    Text deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -124,7 +124,7 @@
                     </div>
 
                     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        Warcaster - Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
             </div>
